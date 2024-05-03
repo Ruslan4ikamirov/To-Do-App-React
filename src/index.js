6c135dbf9c90bde775565086d43af80e0a4a55d0
Ruslan Amirov <rus.amirov.03@bk.ru>
2024-05-03 14:13:24 +0300

    First commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import ToDo from './To-Do';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+      <ToDo />
+  </React.StrictMode>
+);
+
